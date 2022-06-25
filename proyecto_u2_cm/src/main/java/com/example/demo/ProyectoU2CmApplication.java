9aef96fb79f2c9bb74c5dcfb8e2793c6a6e47b1a
Carlos Montalvo <patriciosalas101@gmail.com>
2022-06-24 20:03:54 -0500

    taller 14

--- proyecto_u2_cm/src/main/java/com/example/demo/ProyectoU2CmApplication.java
+++ proyecto_u2_cm/src/main/java/com/example/demo/ProyectoU2CmApplication.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProyectoU2CmApplication {
+public class ProyectoU2CmApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2CmApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("Hola Mundo");
 	}
 
 }
---
+++ proyecto_u2_cm/src/main/java/com/example/repository/IPersonaJdbcRepository.java
@@ -0,0 +1,7 @@
+package com.example.repository;
+
+public interface IPersonaJdbcRepository {
+
+	public 
+	
+}
